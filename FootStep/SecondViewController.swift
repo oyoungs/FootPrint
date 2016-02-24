22851a1bc2fc9720a9d627262fc4c00a07dd688c
Oyoung <oyoung_2012@hotmail.com>
2016-02-24 13:20:25 +0800

    Initial Commit

---
+++ FootStep/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FootStep
+//
+//  Created by oyoung on 16/2/24.
+//  Copyright © 2016年 oyoung. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
